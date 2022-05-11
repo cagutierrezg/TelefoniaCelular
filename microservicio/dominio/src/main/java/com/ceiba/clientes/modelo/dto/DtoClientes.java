ea2c50acfbd82ca2d0703260c0ac12f0855ccf5c
Camila Andrea Gutierrez Guerrero - Ceiba Software <camila.gutierrez@ceiba.com.co>
2022-05-11 15:17:39 -0500

    Se agregan soluciones al sonar

--- microservicio/dominio/src/main/java/com/ceiba/clientes/modelo/dto/DtoClientes.java
+++ microservicio/dominio/src/main/java/com/ceiba/clientes/modelo/dto/DtoClientes.java
@@ -3,12 +3,10 @@ package com.ceiba.clientes.modelo.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 public class DtoClientes {
 
     private Long id;
